bf76e995a67ebc7de0c9bae2c25ab1520a217767
Nicole Hall <nicolealexandrahall@gmail.com>
2016-02-17 15:09:40 -0700

    added test to ensure that followers details method is working properly. Test passes

--- test/services/github_service_test.rb
+++ test/services/github_service_test.rb
@@ -7,16 +7,22 @@ class GithubServiceTest < ActiveSupport::TestCase
       user = create_nicole
       followers = GithubService.new(user).followers_count
       assert_equal 12, followers
+    end
+  end
 
+  test "#followers_details" do
+    VCR.use_cassette('github_service_followers_details') do
+      user = create_nicole
+      followers_list = GithubService.new(user).followers_details
+      assert_equal "mikedao", followers_list.first[0]
+      assert_equal "https://github.com/mikedao", followers_list.first[1]
     end
   end
 
   test "#starred_repos_count" do
     VCR.use_cassette('github_service_starred_repos_count') do
       user = create_nicole
-
       starred_repos = GithubService.new(user).starred_repos_count
-
       assert_equal 0, starred_repos
     end
   end
